22885a06151ea371fbebc22ae2812159c14c9003
David Sena <daviidsena@gmail.com>
2018-07-30 15:41:07 -0300

    Atualizacao do aritmeticos.js

--- Fundamentos/aritmeticos.js
+++ Fundamentos/aritmeticos.js
@@ -4,5 +4,5 @@ const subtracao = a - b
 const multiplicacao = c * d
 const divisao = (d / a).toFixed(2)
 const modulo = c % 2
-
+const divisao = (c / a).toFixed(2)
 console.log(soma, subtracao, multiplicacao, divisao, modulo)
